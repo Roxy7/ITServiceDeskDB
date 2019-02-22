b43da39d68fe64bfb25db9080aa5632ffeca0976
JlF <jlf@xp-it.com>
2019-02-22 10:44:04 +0100

    Adding FK from Agent to AgentGroup and Person

--- ITServiceDeskDB/d_agent.sql
+++ ITServiceDeskDB/d_agent.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[d_agent]
 (
-	[agentId] BIGINT NOT NULL PRIMARY KEY
+	[agentId] BIGINT NOT NULL PRIMARY KEY,
+	[personId] BIGINT NOT NULL,
+    [agentGroupId] BIGINT NULL, 
+	CONSTRAINT [FK_d_agent_d_person] FOREIGN KEY ([personId]) REFERENCES [d_person]([personId]),
+    CONSTRAINT [FK_d_agent_d_agentGroup] FOREIGN KEY ([agentGroupId]) REFERENCES [d_agentGroup]([agentGroupId])
 )
